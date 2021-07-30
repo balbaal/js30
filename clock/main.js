a9197bfc2558373547a1849b1c6d18a7733c097e
Balbaal <iqbal.mi20@gmail.com>
2021-07-30 09:41:13 +0800

    add: second pin logic move

--- clock/main.js
+++ clock/main.js
@@ -1,1 +1,13 @@
-console.log('set date')
+const elSecondPin = document.querySelector(".second-pin");
+
+function setDate() {
+  const dateNow = new Date();
+  const seconds = dateNow.getSeconds();
+  console.log(seconds)
+
+  // transform second to degrees
+  const secondsDeg = ((seconds / 60) * 360) + 90;
+  elSecondPin.style.transform = `rotate(${secondsDeg}deg)`;
+}
+
+setInterval(setDate, 1000);
