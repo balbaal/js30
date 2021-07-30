ed7b26115be02d9d2f713def1509d8b225429124
Balbaal <iqbal.mi20@gmail.com>
2021-07-30 09:50:59 +0800

    add: finishing rotate min, hours js

--- clock/main.js
+++ clock/main.js
@@ -1,13 +1,23 @@
 const elSecondPin = document.querySelector(".second-pin");
+const elMinutePin = document.querySelector(".minute-pin");
+const elHourPin = document.querySelector(".hour-pin");
 
 function setDate() {
   const dateNow = new Date();
   const seconds = dateNow.getSeconds();
-  console.log(seconds)
+  const minutes = dateNow.getMinutes();
+  const hours = dateNow.getHours();
+  console.log(seconds);
 
   // transform second to degrees
-  const secondsDeg = ((seconds / 60) * 360) + 90;
+  const secondsDeg = (seconds / 60) * 360 + 90;
   elSecondPin.style.transform = `rotate(${secondsDeg}deg)`;
+
+  const minutesDeg = (minutes / 60) * 360 + 90;
+  elMinutePin.style.transform = `rotate(${minutesDeg}deg)`;
+
+  const hoursDeg = (hours / 12) * 360 + 90;
+  elHourPin.style.transform = `rotate(${hoursDeg}deg)`;
 }
 
 setInterval(setDate, 1000);
